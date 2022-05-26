82f5cad2ee1d47cb4f6f3e11bb9d8128672c1e2c
Anon1777 <joetenace100@gmail.com>
2022-05-25 23:24:53 -0400

    twenty fifth commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 @app.route("/austria")
 def austria():
